ee6e3ae5858466280f9d15499b25dc8ce254ada4
sps12311072 <sps12311072@gmail.com>
2025-12-01 19:19:39 +0530

    first commit

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,2 @@
 export const PORT = process.env.PORT || 5000;
-export const MONGODB_URL =
-  "mongodb+srv://Arbab:IMGVJBA6S4EkSsDB@cluster0.brxuxmp.mongodb.net/Books-collection?retryWrites=true&w=majority&appName=Cluster0";
+export const MONGODB_URL = "mongodb://localhost:27017/todo-list";
